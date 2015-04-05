668f0dd29fb55694aa5189f42a1984b3a9a8302f
jakubecp <a.headache@seznam.cz>
2015-04-05 18:21:39 +0200

    pokus with commit... still no idea how it works...

--- kliceni.R
+++ kliceni.R
@@ -6,7 +6,7 @@
 #EXAMPLE!!!!!!!!!!!!!!!
 #test for feeding choice experiment (8 ladybirds ate the egg out of 24)
 
-binom.test(8, 24, 0.5)
+
 # test for difference between number of parasitized ladybirds which ate the boiled eggs and which did not (p = 0.38).
 binom.test(7, 10, 0.38)
 #test for difference between number of parasitized ladybirds between wageningen and radwijk
@@ -17,6 +17,18 @@ local
 
 #LITORELA
 
+#test if true probability of germination of seeds covered by 1cm of soil is the same as for the control
+binom.test(7, 68, 0.1333333333)
+
+#test if true probability of germination of seeds covered by 3 and 5 cm of soil is the same as for the control
+binom.test(0, 75, 0.1333333333)
+
+#I have doubts about these test because i am doing multiple comparisons
+#, which is not statisticaly correct. I should try something else...
+
+
+
+#some bulshit - delete it
 local = cbind (c(10,7,0,0), c(65,68,75,75))
 fisher.test (local)
 
