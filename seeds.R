318522ada88790e4fd8ccd325414f659cc3f7d31
Pavel <a.headache@seznam.cz>
2016-01-19 14:06:06 +0100

    gitignore update and some minor changes to functionality

--- seeds.R
+++ seeds.R
@@ -10,10 +10,15 @@ p=data$germ/data$n
 p_lab=data_lab$germ/data_lab$n
 p_ext=data_ext$germ/data_ext$n
 
+install.packages("lazyeval")
+
 if (packageVersion("devtools") < 1.6) {
   install.packages("devtools")
 }
 devtools::install_github("hadley/lazyeval")
 devtools::install_github("hadley/dplyr")
 
+
 # have to restructuralized the data ...
+library(dplyr)
+library (lazyeval)
