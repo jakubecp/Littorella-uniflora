fae3a62cb580e8963ce910b5b37b270637dec2bf
Pavel <a.headache@seznam.cz>
2016-01-21 10:48:52 +0100

    reshaping and analysis of glasshouse germination under the soil layer

--- R/outdoor.R
+++ R/outdoor.R
@@ -5,4 +5,35 @@ library (lazyeval)
 library(tidyr)
 library(reshape2)
 
-data=read.csv ("resubmision/prolicovani_sklenik.csv", header=TRUE, sep=";") 
+data=read.csv ("resubmision/proklicovani_sklenik.csv", header=TRUE, sep=";") 
+
+#treat will be left out and not melted, but rest will be
+substr <- melt (data, c("depth", "dish")) 
+
+head (substr) #see how it looks like
+names (substr) <- c("depth", "dish", "date", "germ") #meaningful names of variables
+#data exploration
+summary (substr)
+mean (substr$germ [substr$depth == "b"])
+str (substr)
+plot (substr$depth, substr$germ)
+
+#results will go into this data frame and will be used for further analysis
+levels (substr$depth)
+treat <- c("a", "b", "c", "d")
+germ <- c(sum (substr$germ [substr$depth == "a"]),
+          sum (substr$germ [substr$depth == "b"]),
+          sum (substr$germ [substr$depth == "c"]),
+          sum (substr$germ [substr$depth == "d"]))
+total <- c(rep (75, 4))
+p=germ/total #probability of succesfull germination
+negative <- total-germ #how many did not germinate
+
+data_glass<- data.frame (treat, germ, total, negative, p) # dataframe of resulting values
+y<-cbind(germ, total - germ) # this vector should be feedid into binomial model
+
+#binomial model and its p-values, "a" is control so every results is compared with control by summary function
+m1=glm(y~treat, family=binomial)
+anova(m1, test="Ch")
+summary (m1)
+
