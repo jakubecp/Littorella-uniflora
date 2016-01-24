af2d2be38694b560d7465b7d64deb1d7d3a471b9
Pavel <a.headache@seznam.cz>
2016-01-24 17:52:25 +0100

    barplots for climabox depth experiment

--- R/climabox.R
+++ R/climabox.R
@@ -1,9 +1,12 @@
 rm(list=ls())
 # reshaping the data to be in tidy format
-library(dplyr)
-library (lazyeval)
-library(tidyr)
-library(reshape2)
+library(dplyr) # manipulation with original data
+library (lazyeval) # manipulation with original data
+library(tidyr) # manipulation with original data
+library(reshape2) # manipulation with original data
+library(ggplot2) # plotting graphs
+library(Rmisc) # summarySE function for SE and CI calcul. and ploting
+library (MASS) # dose.p function for estimation of limitting depth of soil layer
 
 data=read.csv ("resubmision/proklicovani_klima.csv", header=TRUE, sep=";") 
 
@@ -46,6 +49,20 @@ ggplot (data_klima, aes (x=treat, y=p))+
   geom_point(size=3)
 
 #germination decrease with increasing depth of soil layer
-library (MASS)
 summary (m1)
 dose.p (m1, cf=c(1:2), p=seq(0.01,0.9,0.05))
+
+#barplot of mean or median germination success across different substrates.#summarySE is function, which is preparing data to be ploted with SE or confidence intervals...
+sumary.dev = summarySE (substr, measurevar="germ", groupvars="depth")
+
+tiff (filename="outputs/climabox_barplots_depth.tiff", 
+  width=5000, height=3500, 
+  compression="lzw", res= 800)
+p = ggplot (sumary.dev, aes (y=germ, x=depth))
+p + stat_summary(fun.y=mean, geom="bar", position=position_dodge())+
+  xlab("Depth of substrate")+
+  ylab("Mean number of germinated seeds")+
+  geom_errorbar(aes(ymin=germ-se, ymax=germ+se),
+    width=.2,                    # Width of the error bars
+    position=position_dodge(.9))
+dev.off()
